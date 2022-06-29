0cf7d861a04bd3689e228aed662c30662d77ecd2
vishal <vishaldeepverma@live.in>
2022-06-29 20:20:44 +0530

    good added

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+app.get("/good", (req, res) => {
+  res.send("hello good");
+});
+
 app.listen(8080, () => {
   console.log("started");
 });
